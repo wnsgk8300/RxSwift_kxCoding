adec1e269d60d508d87e529043953059d018d600
wnsgk8300 <wnsgk8300@gmail.com>
2023-01-11 16:45:39 +0900

    Relay

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/Relay.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/Relay.playground/Contents.swift
@@ -32,8 +32,32 @@ import RxCocoa
 let bag = DisposeBag()
 
 
+let prelay = PublishRelay<Int>()
 
+prelay.subscribe { print("1: \($0)") }
+    .disposed(by: bag)
 
+// relay로 next이벤트 전달할 땐 accept를 사용한다
+prelay.accept(1)
 
+let brelay = BehaviorRelay(value: 1)
 
+brelay.accept(2)
 
+brelay.subscribe { print("2: \($0)") }
+    .disposed(by: bag)
+
+brelay.accept(3)
+
+//value: relay의 next이벤트에 접근에서 저장한 값을 가져온다
+print(brelay.value) // 3
+
+brelay.accept(4)
+print(brelay.value) // 4
+
+let rrelay = ReplayRelay<Int>.create(bufferSize: 3)
+
+(1...10).forEach { rrelay.accept($0) }
+
+rrelay.subscribe { print("3: \($0)") }
+    .disposed(by: bag)
